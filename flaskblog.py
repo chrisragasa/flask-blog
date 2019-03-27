b500d00bbf3eb636ce4253deba3f5f089fbf78d8
Christopher Ragasa <chrisragasa@Christophers-MacBook-Pro-3.local>
2019-03-26 22:57:13 -0700

    create database with Flask-SQLAlchemy

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,36 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
+from datetime import datetime
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a67b8f927998c2032dd4620a7359d5b4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+# represent database model as a class
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True) # additional query that grabs this user object from a post
+
+    # how object is printed
+    def __repr__(self):
+        return f"User('{self.id}', '{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) # pass in the datetime.utcnow function without ()
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    # how object is printed
+    def __repr__(self):
+        return f"User('{self.id}', '{self.title}', '{self.date_posted}', '{self.content}')"
 
 posts = [
     {
